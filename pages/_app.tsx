1912e1e8f2f81dc9f56ba8d40ec5917f7482f80c
Cattle <cattle@192.168.0.100>
2022-10-13 16:18:39 +0800

    bff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps, AppContext } from "next/app";
+import App from "next/app";
+import { Layout, ILayoutProps } from "@/components/layout";
+import code from "@/public/code.png";
+import Head from "next/head";
+import axios from "axios";
+import { LOCALDOMAIN } from "@/utils";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = (data: AppProps & ILayoutProps) => {
+  const { Component, pageProps, navbarData, footerData } = data;
 
-export default MyApp
+  return (
+    <div>
+      <Head>
+        <title>A Demo for SSR</title>
+        <meta name="description" content="A Demo for SSR" />
+        <link rel="stylesheet" href="/favicon.io" />
+      </Head>
+      <Layout navbarData={navbarData} footerData={footerData}>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
+};
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const pageProps = await App.getInitialProps(context);
+  const { data = {} } = await axios.get(`${LOCALDOMAIN}/api/layout`);
+
+  return {
+    ...pageProps,
+    ...data,
+  };
+};
+
+export default MyApp;
